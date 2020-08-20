6605fc2c19fa5ed20da563df6e788f26389aa293
ahmet <ahmetsalihozm@outlook.com>
2020-08-20 18:23:27 +0300

    changing fetch address

--- pages/index.js
+++ pages/index.js
@@ -74,11 +74,10 @@ import fetch from 'isomorphic-unfetch';
 }
 
 Home.getInitialProps = async ({req}) => {
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch("https://ahmetozmen.herokuapp.com/api/posts");
   const json = await res.json();
   json.reverse();
   return { posts: json };
 };
 
 export default Home;
-// https://ahmetozmen.herokuapp.com
